7e294924eb763bce5c46bd5ca791f326ca4bdb8e
Fatih UNAL <fatihbe25@gmail.com>
2022-03-02 11:21:25 +0100

    migratie fout

---
+++ Model/Afdeling.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    public class Afdeling
+    {
+        public int AfdelingId { get; set; }
+        public string AfdelingCode { get; set; }
+        public string AfdelingNaam { get; set; }
+        public string AfdelingTekst { get; set; }
+
+        public virtual ICollection<Medewerker> Medewerkers { get; set; }
+    }
+
+}
--- Model/Medewerker.cs
+++ Model/Medewerker.cs
@@ -15,14 +15,4 @@ namespace Model
 
     }
 
-    public class Afdeling
-    {
-        public int AfdelingId { get; set; }
-        public string AfdelingCode { get; set; }
-        public string AfdelingNaam { get; set; }
-        public string AfdelingTekst { get; set; }
-
-        public virtual ICollection<Medewerker> Medewerkers { get; set; }
-    }
-
 }
